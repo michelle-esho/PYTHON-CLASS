747625dadea500f34e28650c3a931c821106cc08
michelle-esho <eshomichelle@gmail.com>
2026-09-09 10:57:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -64,3 +64,36 @@ def create_account():
             return name, balance
         except ValueError:
             print("Please enter a valid amount.")
+
+
+def check_balance(balance):
+ print(f"\nCurrent Balance: ■{balance:,.2f}")
+
+
+def deposit(balance):
+    try:
+        amount = float(input("Enter amount to deposit: ■"))
+        if amount <= 0:
+            print("Amount must be greater than zero.")
+            return balance
+        balance += amount
+        print(f"■{amount:,.2f} deposited successfully.")
+    except ValueError:
+        print("Invalid amount.")
+
+
+def withdraw(balance):
+    try:
+        amount = float(input("Enter amount to withdraw: ■"))
+        if amount <= 0:
+            print("Amount must be greater than zero.")
+        elif amount > balance:
+            print("Insufficient balance.")
+        else:
+            balance -= amount
+            print(f"■{amount:,.2f} withdrawn successfully.")
+    except ValueError:
+        print("Invalid amount.")
+        return balance  
+
+
